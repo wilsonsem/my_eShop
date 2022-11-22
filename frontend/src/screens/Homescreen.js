66ebaf6a9c1ddf89ad47d5ed41a326334d7a1f9a
Semmwa <bakshaksemmwa@gmail.com>
2022-11-22 04:08:29 +0100

    Rating component error fixed

--- frontend/src/screens/Homescreen.js
+++ frontend/src/screens/Homescreen.js
@@ -10,7 +10,6 @@ const Homescreen = () => {
       <Row>
         {products.map(product => (
           <Col sm={12} md={6} lg={4} xl={3}>
-            {/* <h3>{product.name}</h3> */}
             <Product product = {product} />
           </Col>
         ))}
