c367fb32a6318f7417674854c84df2fc9d466b73
Semmwa <bakshaksemmwa@gmail.com>
2022-11-24 16:54:50 +0100

    serving products from backend

---
+++ backend/server.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const products = require('./data/products')
+
+
+
+const app = express()
+
+
+app.get('/', (req,res) => {
+    res.send('API is running')
+})
+app.get('/products', (req, res) => {
+    res.json(products)
+})
+
+app.get('/products/:id', (req, res) => {
+
+    const{ id } = req.params
+    
+    const singleproduct = products.find((p) => p._id === Number(id))
+    res.json(singleproduct)
+})
+
+app.listen(4000, () => {
+    console.log("Eshop listening on port 4000")
+})
