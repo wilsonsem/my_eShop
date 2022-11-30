4ea7cd9c6e30cb8b1a5e361415ba3dd8739352d2
Semmwa <bakshaksemmwa@gmail.com>
2022-11-30 08:57:30 +0100

    fixed connection error

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,18 +3,12 @@ import mongoose from "mongoose"
 const connectDB = async () => {
 
     try{
-        const conn = await mongoose.connect(process.env.DB_URI, {
-            useUnifiedTopology:rrue,
-            useNewUrlParser: true,
-            useCreateIndex: true
-        })
+        const conn = await mongoose.connect(process.env.DB_URI)
         console.log(`Database connected: ${conn.connection.host}`)
     }
     catch(error){
         console.log("Not connected")
-        // process.exit(1)
     }
 }
-
 
 export default connectDB
