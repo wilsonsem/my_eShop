f5d3be88f355492da5b531989ab00bf75759f9ed
Semmwa <bakshaksemmwa@gmail.com>
2022-12-16 10:53:25 +0100

    Creating redux web store

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider} from 'react-redux'
+import store from './store';
 import './index.css';
 import './bootstrap.min.css'
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// const store = configureStore({ allReducers });
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <Provider store ={store}>
     <App />
-  </React.StrictMode>
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
---
+++ frontend/src/store.js
@@ -0,0 +1,35 @@
+import { combineReducers, applyMiddleware, createStore } from 'redux'
+// import { configureStore } from '@reduxjs/toolkit'
+// import { configureStore } from '@reduxjs/toolkit'
+// import rootReducer from './reducers'
+import thunk from 'redux-thunk'
+import { composeWithDevTools } from '@redux-devtools/extension'
+
+
+const reducer = combineReducers({})
+
+const initialState = {}
+
+const middleware = [thunk]
+
+// const store = configureStore(
+//     reducer,
+//     initialState,
+//     composeWithDevTools(applyMiddleware(...middleware)))
+
+
+const store = createStore(
+    reducer,
+    initialState,
+    composeWithDevTools(applyMiddleware(...middleware)))
+
+
+
+    
+    // const store = configureStore({
+    //   reducer: rootReducer
+    // })
+    
+    // export default store
+
+export default store
