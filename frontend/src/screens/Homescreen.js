853d33655ecd486364a9bd7b10bfd755ccaf046e
Semmwa <bakshaksemmwa@gmail.com>
2022-12-17 04:20:24 +0100

    bringing redux state into homwscreen

--- frontend/src/screens/Homescreen.js
+++ frontend/src/screens/Homescreen.js
@@ -1,24 +1,27 @@
 import React, {useState, useEffect } from "react"
 import { Row, Col} from "react-bootstrap"
+import { useDispatch, useSelector} from 'react-redux'
 import Product from "../components/Product"
-import axios from 'axios'
+import {listProducts} from '../actions/productActions'
 
 const Homescreen = () => {
-  const [products, setProducts] = useState([])
+  const dispatch = useDispatch()
 
- useEffect(() => {
-  const fetchProducts = async () =>{
-    const { data } = await axios.get('/api/products')
+  const productList = useSelector( state => state.productList)
+  const {loading, error, products} = productList
 
-    setProducts(data)
-  }
-  
-  fetchProducts()
- }, [])
+  useEffect(() => {
+
+    dispatch(listProducts())
+
+  }, [dispatch])
 
   return (
     <>
       <h1>Latest products</h1>
+      { 
+      loading ? <h2>Loading ...</h2> : 
+      error ? <h3>{error}</h3> :
       <Row>
         {products.map(product => (
           <Col key= {product._id} sm={12} md={6} lg={4} xl={3}>
@@ -26,6 +29,8 @@ const Homescreen = () => {
           </Col>
         ))}
       </Row>
+     }
+      
     </>
   )
 }
