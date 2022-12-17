4e0af9d76d043383b51b479609b4acaa23670fef
Semmwa <bakshaksemmwa@gmail.com>
2022-12-17 05:10:34 +0100

    adding the product details actions

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -24,3 +24,22 @@ export const listProducts = () => async (dispatch) => {
         })
     }
 }
+
+export const listProductDetail = (id) => async (dispatch) => {
+    try {
+        dispatch({ type: PRODUCT_DETAILS_REQUEST})
+
+        const { data } = await axios.get(`/api/products/${id}`)
+
+        dispatch({
+            type: PRODUCT_DETAILS_SUCCESS,
+            payload : data
+        })
+    } catch (error) {
+        
+        dispatch({
+            type : PRODUCT_DETAILS_FAIL,
+            payload: error.response && error.response.data.message ? error.response.data.message :error.message,
+        })
+    }
+}
