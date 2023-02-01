804f4dfcd220ff59d712e11ab044e1372bb1a299
Semmwa <bakshaksemmwa@gmail.com>
2023-02-01 11:48:31 +0100

    user registration and password encryption done

--- backend/controllers/userContoller.js
+++ backend/controllers/userContoller.js
@@ -21,6 +21,37 @@ const authUser = asyncHandler(async(req , res) => {
     }
 })
 
+//Register a new User
+
+const registerUser = asyncHandler(async(req, res) => {
+
+    const { name, email, password } = req.body
+
+    const userExists = await User.findOne({ email })
+    if(userExists){
+        res.status(400)
+        throw new Error('User already exist')
+    }
+
+    const user = await User.create({
+        name, email, password
+    })
+
+    if(user){
+        res.status(201).json({
+            _id : user._id,
+            name : user.name,
+            email: user.email,
+            isAdmin: user.isAdmin,
+            token: generateToken(user._id)
+        })
+    }else{
+        res.status(400)
+        throw new Error('Invalid user data')
+    }
+
+})
+
 
 const getUserProfile = asyncHandler(async(req , res) => {
     const user = await User.findById(req.user._id)
@@ -41,5 +72,6 @@ const getUserProfile = asyncHandler(async(req , res) => {
 
 export {
     authUser,
+    registerUser,
     getUserProfile
 }
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,9 +1,10 @@
 import express from 'express'
-import { authUser, getUserProfile} from '../controllers/userContoller.js'
+import { authUser, getUserProfile, registerUser} from '../controllers/userContoller.js'
 import{ protect } from '../middleware/authMiddleware.js'
 
 const router = express.Router()
 
+router.post('/', registerUser)
 router.post('/login', authUser)
 router.get('/profile', protect, getUserProfile)
 // router.route('/profile').get( protect, getUserProfile)
